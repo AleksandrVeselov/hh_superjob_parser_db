85480a5f94d681cda9898c93bb9442387a631b35
Aleksandr Veselov <sanek_ves@mail.ru>
2023-05-09 20:00:55 +0300

    Написан класс HeadHunterAPI

--- classes.py
+++ classes.py
@@ -1,19 +1,21 @@
 import requests
+
+
 class HeadHunterAPI:
     """Класс определяющий функционал для работы с api сайта HeadHunter"""
-    URL = 'https://api.hh.ru/vacancies'
+    URL = 'https://api.hh.ru/vacancies'  # URL для поиска вакансий
 
-    def get_request(self, company, page, per_page=100):
+    def get_request(self, employer_id, page, per_page=100):
         """
         Отправка запроса на API
-        :param company: название компании
+        :param employer_id: id компании работодателя
         :param page: номер страницы
         :param per_page: количество вакансий на одной странице
         :return: json со списком вакансий
         """
 
-        # в параметрах задана сортировка по дате и только с указанной зарплатой в рублях по России
-        params = {'text': keyword,
+        # в параметрах задана сортировка по дате
+        params = {'employer_id': employer_id,
                   'page': page,
                   'per_page': per_page,
                   'order_by': "publication_time",
@@ -22,19 +24,33 @@ class HeadHunterAPI:
         response = requests.get(self.URL, params=params).json()
         return response['items']
 
-    def get_vacancies(self, company: str) -> list[json]:
+    def get_vacancies(self, employer_id: int) -> list[dict]:
         """
-
-        :param company: компания, для которой нужно получить список вакансий
+        :param employer_id: id компании работодателя, для которой нужно получить список вакансий
         :return: список с вакансиями на соответствующей странице
         """
-        # Максимальное количество вакансий для парсинга - 2000
-        if pages > 20:
-            raise ValueError('Вы превысили максимальное число вакансий, возможных для парсинга по API')
-
         vacancies = []  # список с вакансиями
-        for page in range(pages):
-            page = self.get_request(keyword, page, area)
+        for page in range(20):
+            page = self.get_request(employer_id, page)
+            if not page:
+                # Если в запросе нет вакансий, завершаем цикл
+                break
             vacancies.extend(page)
 
         return vacancies
+
+    @staticmethod
+    def get_employer_id(employer: str) -> list[dict]:
+        """
+        Получение информации о компании для дальнейшего использования в методах класса
+        :param employer:
+        :return:
+        """
+        url = 'https://api.hh.ru/employers'  # URL для поиска работодателей
+        params = {'text': employer,
+                  'per_page': 100,
+                  }
+
+        response = requests.get(url, params=params).json()
+        return response['items']
+
--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import requests
+
+from classes import HeadHunterAPI
+
 
 def main():
     """"""
+    companies = [{'name': 'Ключевые системы и компоненты', 'id': 3403877},
+                 {'name': 'Skyeng', 'id': 1122462},
+                 {'name': 'СБЕР', 'id': 3529},
+                 {'name': 'ВТБ', 'id': 4181},
+                 {'name': 'Иннотех, Группа компаний', 'id': 4649269},
+                 {'name': 'Ростелеком', 'id': 2748},
+                 {'name': 'РОСБАНК', 'id': 599},
+                 {'name': 'ВсеИнструменты.ру', 'id': 208707},
+                 {'name': 'билайн', 'id': 4934},
+                 {'name': 'МТС', 'id': 3776}]  # Список компаний для парсинга
+    hh_api = HeadHunterAPI()
+    print(*hh_api.get_employer_id('ПАО МТС'), sep='\n')
+    # vac = hh_api.get_vacancies(1122462)  # список словарей с открытыми вакансиями компании по ключу vacancies_url
+    # print(*vac, sep='\n')
 
 
 
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     main()
