2bcf4b767b6c5ce3df5324488397ac9e753e18b4
Aleksandr Veselov <sanek_ves@mail.ru>
2023-05-12 19:33:52 +0300

    Изменен список окмпаний для парсинга

--- main.py
+++ main.py
@@ -11,30 +11,30 @@ def main():
     """Главный скрипт проекта"""
 
     companies = [{'name': 'Ключевые системы и компоненты', 'id': 3403877},
-                 {'name': 'Skyeng', 'id': 1122462},
-                 {'name': 'СБЕР', 'id': 3529},
-                 {'name': 'ВТБ', 'id': 4181},
+                 {'name': 'Тензор', 'id': 67611},
+                 {'name': 'SberTech', 'id': 906557},
+                 {'name': 'Rambler', 'id': 8620},
                  {'name': 'Иннотех, Группа компаний', 'id': 4649269},
                  {'name': 'Ростелеком', 'id': 2748},
-                 {'name': 'РОСБАНК', 'id': 599},
-                 {'name': 'МТС', 'id': 3776},
+                 {'name': 'Первый Бит', 'id': 3177},
+                 {'name': 'IBS', 'id': 139},
                  {'name': 'Университет Иннополис', 'id': 1160188},
-                 {'name': 'Softline', 'id': 2381}]  # Список компаний для парсинга
+                 {'name': 'Softline', 'id': 2381}]  # Список компаний для парсинга Первый Бит Skillbox BIA Technologies
 
     params = config()  # Параметры для подключения к базе данных
     hh_api = HeadHunterAPI()  # Инициализация класса для работы с сайтом Headhunter
-
-    utils.create_database(DB_NAME, params)  # создание базы данных
-    params.update({'dbname': DB_NAME})  # обновление параметров для подключения к базе данных
-    print(f"БД успешно создана")
-    utils.create_table(TABLE_NAME, params)  # Создание таблицы для заполнения в базе данных
-    print("Таблица успешно создана")
-
-    # заполнение таблицы данными о вакансиях компании
-    for company in companies:
-        vac = hh_api.get_vacancies(company['id'], VACANCIES_COUNT)  # получение открытых вакансий соответствующей компании
-        utils.add_data_to_database(TABLE_NAME, vac, params)
-        print(f"Таблица успешно заполнена вакансиями компании {company['name']}")
+    print(hh_api.get_employer_id('IBS', 0))
+    # utils.create_database(DB_NAME, params)  # создание базы данных
+    # params.update({'dbname': DB_NAME})  # обновление параметров для подключения к базе данных
+    # print(f"БД успешно создана")
+    # utils.create_table(TABLE_NAME, params)  # Создание таблицы для заполнения в базе данных
+    # print("Таблица успешно создана")
+    #
+    # # заполнение таблицы данными о вакансиях компании
+    # for company in companies:
+    #     vac = hh_api.get_vacancies(company['id'], VACANCIES_COUNT)  # получение открытых вакансий соответствующей компании
+    #     utils.add_data_to_database(TABLE_NAME, vac, params)
+    #     print(f"Таблица успешно заполнена вакансиями компании {company['name']}")
 
 
 if __name__ == '__main__':
